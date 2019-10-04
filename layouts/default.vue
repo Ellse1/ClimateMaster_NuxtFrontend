5c142b0106953772d1b304a3beeb9b00b61ce9f5
Elias Singer <elias.singer@online.de>
2019-10-04 16:43:34 +0200

    LogIn Functionality nice

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,17 @@
 <template>
   <div>
-    <nuxt />
 
-    <div class="text-center">
-      <nuxt-link to="/" exact-active-class="active">Home</nuxt-link> |
-      <nuxt-link to="/HandlungsvorschalgHinzufuegen" exact-active-class="active">Handlungsmoeglichkeit hinzufuegen</nuxt-link> |
-      <nuxt-link to="/login" exact-active-class="active">login</nuxt-link>
+    <div v-if="loggedIn" class="text-center">
+      <h3>Hallo {{user.name}}</h3>
     </div>
+
+    <nuxt />
   
-                    <template v-if="!loggedIn">
-                        <li class="nav-item">
-                            <nuxt-link to="/login" class="nav-link">
-                                Login
-                            </nuxt-link>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="">Register</a>
-                        </li>
-                    </template>
-                    <template v-if="loggedIn">
-                        <li class="nav-item">
-                            <a class="nav-link" @click.prevent="logout" href="#">
-                                    Logout
-                            </a>  
-                        </li>
-                    </template>
+    <div v-if="loggedIn" class="text-center"> 
+        <nuxt-link to="/HandlungsvorschalgHinzufuegen" exact-active-class="active">Handlungsmoeglichkeit hinzufuegen</nuxt-link> |
+        <nuxt-link to="/" exact-active-class="active">Home</nuxt-link> |
+        <a @click.prevent="logout" href="#">logout</a>
+    </div>
 
   </div>
 </template>
