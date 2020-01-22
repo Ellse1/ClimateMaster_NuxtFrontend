f9f4a4db826a667fe48efdba66edaad72e510f14
Elias Singer <elias.singer@online.de>
2020-01-22 11:27:57 +0100

    show addCompany

--- components/ClimadviceContent/buyGreenElectricity/buyGreenElectricity.vue
+++ components/ClimadviceContent/buyGreenElectricity/buyGreenElectricity.vue
@@ -1,4 +1,13 @@
 <template>
     <div>
+        <slideshowOfCompaniesOfClimadviceName :climadvice_name="'climateFriendlyInvestment'" />
     </div>
 </template>
+<script>
+import slideshowOfCompaniesOfClimadviceName from '~/components/Company/slideshowOfCompaniesOfClimadviceName';
+export default {
+    components:{
+        slideshowOfCompaniesOfClimadviceName
+    }
+}
+</script>
--- components/ClimadviceContent/climateFriendlyHeating/climateFriendlyHeating.vue
+++ components/ClimadviceContent/climateFriendlyHeating/climateFriendlyHeating.vue
@@ -1,5 +1,13 @@
 <template>
     <div>
-        
+        <slideshowOfCompaniesOfClimadviceName :climadvice_name="'climateFriendlyHeating'" />
     </div>
 </template>
+<script>
+import slideshowOfCompaniesOfClimadviceName from '~/components/Company/slideshowOfCompaniesOfClimadviceName';
+export default {
+    components:{
+        slideshowOfCompaniesOfClimadviceName
+    }
+}
+</script>
