04144ae759de6773e5b64fb019f63493dc32b3f7
Elias Singer <elias.singer@online.de>
2020-01-29 10:27:49 +0100

    my profile in container

--- pages/account/myProfile.vue
+++ pages/account/myProfile.vue
@@ -1,81 +1,84 @@
 <template>
-    <div class="container bg-light pt-4" style="min-height:400px;">
-        <h2 class="text-center">Mein ClimateMaster Profil</h2>
+    <div class="container">
 
-        <hr class="col-md-5 mx-auto">
+        <div class="container bg-light pt-4" style="min-height:400px;">
+            <h2 class="text-center">Mein ClimateMaster Profil</h2>
 
-        <!-- For profile picture -->
-        <div class="text-center ">
-            <h4>Profilbild</h4>
-            <label for="id_profilePicture">
-                <div class="border border-dark pl-3 pr-3 pt-3" style="border-radius:50px;" id="id_div_uploadProfilePicture">
-                    <!-- icon if no profile_picture -->
-                    <font-awesome-icon v-if="profile_picture_name == null" icon="user-circle" id="id_icon_profile_picture" style="font-size: 200px;"/> 
-                    <div id="id_div_loading" class="bg-success rounded mt-2 mb-2" style="width:50%;margin:auto;"></div>
-                    <!-- image if profile picture available -->
-                    <div  id="id_div_profilePicture" style="width:200px;height:200px;border-radius:50%;overflow:hidden;display:none;" class="border bg-dark">
-                        <img id="id_img_profilePicture" style="height:100%;">
+            <hr class="col-md-5 mx-auto">
+
+            <!-- For profile picture -->
+            <div class="text-center ">
+                <h4>Profilbild</h4>
+                <label for="id_profilePicture">
+                    <div class="border border-dark pl-3 pr-3 pt-3" style="border-radius:50px;" id="id_div_uploadProfilePicture">
+                        <!-- icon if no profile_picture -->
+                        <font-awesome-icon v-if="profile_picture_name == null" icon="user-circle" id="id_icon_profile_picture" style="font-size: 200px;"/> 
+                        <div id="id_div_loading" class="bg-success rounded mt-2 mb-2" style="width:50%;margin:auto;"></div>
+                        <!-- image if profile picture available -->
+                        <div  id="id_div_profilePicture" style="width:200px;height:200px;border-radius:50%;overflow:hidden;display:none;" class="border bg-dark">
+                            <img id="id_img_profilePicture" style="height:100%;">
+                        </div>
+                        
+                        <small id="id_message_typeAndSize_profilePicture" class="text-danger">
+                            jpg, png, max:2MB<br>
+                        </small>
+                        <small>
+                            Bild hochladen
+                        </small>
+                        
                     </div>
-                    
-                    <small id="id_message_typeAndSize_profilePicture" class="text-danger">
-                        jpg, png, max:2MB<br>
-                    </small>
-                    <small>
-                        Bild hochladen
-                    </small>
-                    
+                </label>
+                <input type="file" class="mt-2" ref="image" id="id_profilePicture" name="profilePicture" v-on:change="handleImageUpload" style="display:none;"/>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6 mt-4">
+                    <h4 class="text-center">Meine Persönlichen Daten</h4>
+                    <div class="row">
+                        <div class="col-6">
+                            <input class="form-control text-center" v-model="firstname" disabled/>
+                        </div>
+                        <div class="col-6">
+                            <input class="form-control text-center" v-model="lastname" disabled/>
+                        </div>
+                        <div class="col-12 mt-3">
+                            <input class="form-control text-center" v-model="username" disabled/>
+                        </div>
+                        <div class="col-12 text-center mt-3">
+                            <input class="form-control text-center" v-model="email" disabled/>
+                        </div>
+                    </div>
                 </div>
-            </label>
-            <input type="file" class="mt-2" ref="image" id="id_profilePicture" name="profilePicture" v-on:change="handleImageUpload" style="display:none;"/>
-        </div>
 
-        <div class="row">
-            <div class="col-md-6 mt-4">
-                <h4 class="text-center">Meine Persönlichen Daten</h4>
-                <div class="row">
-                    <div class="col-6">
-                        <input class="form-control text-center" v-model="firstname" disabled/>
-                    </div>
-                    <div class="col-6">
-                        <input class="form-control text-center" v-model="lastname" disabled/>
-                    </div>
-                    <div class="col-12 mt-3">
-                        <input class="form-control text-center" v-model="username" disabled/>
-                    </div>
-                    <div class="col-12 text-center mt-3">
-                        <input class="form-control text-center" v-model="email" disabled/>
+                <div class="col-md-6 mt-4">
+                    <h4 class="text-center">Mein zu Hause</h4>
+                    <div class="row">
+                        <div class="col-9">
+                            <input class="form-control text-center" v-model="street" placeholder="Staße"/>
+                        </div>
+                        <div class="col-3">
+                            <input class="form-control text-center" v-model="house_number" placeholder="Nr."/>
+                        </div>
+                        <div class="col-4 text-center mt-3">
+                            <input type="number" class="form-control text-center" v-model="postcode" placeholder="Plz."/>
+                        </div>
+                        <div class="col-8 text-center mt-3">
+                            <input class="form-control text-center" v-model="residence" placeholder="Wohnort" />
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <div class="col-md-6 mt-4">
-                <h4 class="text-center">Mein zu Hause</h4>
-                <div class="row">
-                    <div class="col-9">
-                        <input class="form-control text-center" v-model="street" placeholder="Staße"/>
-                    </div>
-                    <div class="col-3">
-                        <input class="form-control text-center" v-model="house_number" placeholder="Nr."/>
-                    </div>
-                    <div class="col-4 text-center mt-3">
-                        <input type="number" class="form-control text-center" v-model="postcode" placeholder="Plz."/>
-                    </div>
-                    <div class="col-8 text-center mt-3">
-                        <input class="form-control text-center" v-model="residence" placeholder="Wohnort" />
-                    </div>
-                </div>
+            <div class="col-md-12 mt-4 text-center" >
+                <button id="id_button_save" class="btn btn-success mb-4 " style="width:90%" v-on:click="saveAddress">Änderungen speichern</button>
+            
+                <notification :message="error" v-if="error" class="text-danger mt-3" />
+                <notification :message="success" v-if="success" class="text-success mt-3" />
             </div>
-        </div>
 
-        <div class="col-md-12 mt-4 text-center" >
-            <button id="id_button_save" class="btn btn-success mb-4 " style="width:90%" v-on:click="saveAddress">Änderungen speichern</button>
+            <co2calculationChart class="mt-4" />
         
-            <notification :message="error" v-if="error" class="text-danger mt-3" />
-            <notification :message="success" v-if="success" class="text-success mt-3" />
         </div>
-
-        <co2calculationChart class="mt-4" />
-
     </div>
 </template>
 <script>
