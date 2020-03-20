46828f707418b6c18af7018688efb261c1d203b4
Elias Singer <elias.singer@online.de>
2020-03-20 20:04:04 +0100

    climatemaster "starter" -> "Starter"

--- pages/perfect/_username.vue
+++ pages/perfect/_username.vue
@@ -21,7 +21,7 @@
             </div>
             <!-- if ClimateMaster starter -->
             <div v-if="climatemaster_state == 'climatemaster_starter'">
-                <h1><span class="text-success">Climate</span>Master starter 2020</h1>
+                <h1><span class="text-success">Climate</span>Master Starter 2020</h1>
             </div>
 
             <!-- information general -->
