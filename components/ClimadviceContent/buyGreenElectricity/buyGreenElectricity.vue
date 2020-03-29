f4d0f39165f0ed6c70fd102c05e1c16ca9734dad
Elias Singer <elias.singer@online.de>
2020-03-29 12:35:15 +0200

    buyGreenElectricity -> text

--- components/ClimadviceContent/buyGreenElectricity/buyGreenElectricity.vue
+++ components/ClimadviceContent/buyGreenElectricity/buyGreenElectricity.vue
@@ -1,6 +1,17 @@
 <template>
     <div>
-        <slideshowOfCompaniesOfClimadviceName :climadvice_name="'climateFriendlyInvestment'" />
+        <p>
+            Durch die Wahl eines Ökostromanbieters kauft man 100% Strom aus klimafreundlichen, erneuerbaren Energien. 
+            Der Strom wird also mit Photovoltaikanlagen, Windkraftanlagen, in Wasserkraftwerken oder mit anderen umweltfreundlichen,
+            sauberen Technologien produziert. Der Strompreis für den Käufer unterscheidet sich dabei nicht merklich von
+            anderen Anbietern. Viele deutsche Stromkunden sind noch beim Grundversorger und haben Ihren Stromanbieter
+            noch nicht selbst gewählt. Für diese gilt: Grüner Strom ist billiger als der Strom, der vom Grundversorger bzw. 
+            Netzbetreiber angeboten wird. Ein Vertragswechsel lohnt sich.
+        </p>
+
+
+        <slideshowOfCompaniesOfClimadviceName :climadvice_name="'buyGreenElectricity'" />
+
     </div>
 </template>
 <script>
