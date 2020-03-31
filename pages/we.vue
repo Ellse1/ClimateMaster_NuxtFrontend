e2c65d21e0efcd9a0a35b0e0bc40214652764537
Elias Singer <elias.singer@online.de>
2020-03-31 22:51:17 +0200

    we, better design

--- pages/we.vue
+++ pages/we.vue
@@ -1,40 +1,34 @@
 <template>
     <div>
         <div class="container">
-            <div class="container text-center mt-3 col-md-10 bg-light border rounded pb-1 border-success">
-                <h2>Unser Team</h2>
+            <div class="container text-center mt-3 col-md-10 bg-light  rounded p-3">
+                <h4>Unser Team</h4>
 
                 <div class="container" style="max-height:500px;overflow:hidden;">
                     <img class="img-fluid" src="~/static/pictures/forSpecialSites/we/climatemaster_we_3.jpg" alt="Leider kein Bild gefunden">
                 </div>
-                <!-- <div class="row">
-                    <div class="col-md-6 mt-3">
-                        <div class="card">
-                            <img class="card-img-top" src="~/static/pictures/forSpecialSites/we/Elias.jpg" alt="Leider kein Bild" class="card-img-top" style="">
-                            <div class="card-footer text-center">
-                                Elias Singer<br>
-                                ClimateMaster 2020<br>
-                                Development & Marketing
-                            </div>
-                        </div>
-                    </div>
 
-                    <div class="col-md-6 mt-3">
-                        <div class="card">
-                    
-                            <img class="card-img-top" src="~/static/pictures/forSpecialSites/we/Joshua.jpg" alt="Leider kein Bild" class="card-img-top" style="object-fit:cover;">
-                            <div class="card-footer text-center">
-                                Joshua Singer<br>
-                                ClimateMaster 2020<br>
-                                Konzept Manager & Marketing
-                            </div>
-                        </div>
-                    </div>
-                </div> -->
+
+                <h4 class="mt-3">Unser Antrieb</h4>
+                <p class="mt-2">Der Klimawandel stellt eine der größten Herausforderungen unserer Zeit dar, unsere Generation ist direkt 
+                    von den Folgen einer globalen Erwärmung betroffen. An uns liegt es auch etwas zu verändern.<br>
+                    Mit unserer Plattform wollen wir privaten Klimaschutz unterstützen, vereinfachen und würdigen, 
+                    um das Gefühl der Machtlosigkeit zu verhindern und positive Selbstwirksamkeit transparent zu belegen.
+                    Jeder kann heute schon klimaneutral leben und damit die Zukunft folgender Generation verbessern.
+                    Unsere Plattform verknüpft die Analyse des individuellen Ausstoßes von Treibhausgasen mit der 
+                    Reduktionsmöglichkeit und der anschließenden Kompensationsmöglichkeit.
+                </p>
+
+                <div class="text-center">
+                    <a target="_blanc" href="https://www.climate-master.com:8000/api/file/conceptSummary" class="btn btn-default border mt-2">Konzept Zusammenfassung (PDF)</a>
+                    <a target="_blanc" href="https://www.climate-master.com:8000/api/file/concept" class="btn btn-default border mt-2">Konzept Langfassung (PDF)</a>
+                </div>
+
+
             </div>
 
 
-
+<!-- 
 
             <div class="container text-center mt-3 mb-5 bg-light border border-success rounded p-5 col-md-8" style="">
                 <h5>Unser Antrieb</h5>
@@ -51,19 +45,18 @@
                 <div class="text-center">
                     <a target="_blanc" href="https://www.climate-master.com:8000/api/file/conceptSummary" class="btn btn-default border mt-2">Konzept Zusammenfassung (PDF)</a>
                     <a target="_blanc" href="https://www.climate-master.com:8000/api/file/concept" class="btn btn-default border mt-2">Konzept Langfassung (PDF)</a>
-
                 </div>
-                <!-- <div class="col-6 text-center">
+                <div class="col-6 text-center">
                     <a target="_blanc" href="http://localhost:8000/api/file/concept" class="btn btn-default border">Unser Konzept (PDF)</a>
                 </div> -->
             
-            </div>
+            <!-- </div> --> 
 
             <!-- Impressum -->
-            <div class="container text-center mt-3 mb-5 bg-light border border-success rounded p-5 col-md-10" style="">
-                <h5>Impressum</h5>
-                <div class="row">
-                    <div class="col-md-6 text-left">
+            <div class="container text-center mt-3 mb-5 bg-light rounded p-3 col-md-10" style="">
+                <h4>Impressum</h4>
+                <div class="row mt-2">
+                    <div class="col-md-6 text-center">
                         <p>Elias Singer</p>
                         <p>Schulstraße 34</p>
                         <p>78337 Öhningen - Schienen</p>
