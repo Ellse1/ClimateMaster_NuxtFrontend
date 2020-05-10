87ef2a97eb547dca91fb0e639868991e3da51ce4
Elias Singer <elias.singer@online.de>
2020-05-10 14:26:38 +0200

    dont fly climadvice

--- components/ClimadviceContent/dontFly/dontFly.vue
+++ components/ClimadviceContent/dontFly/dontFly.vue
@@ -1,4 +1,7 @@
 <template>
     <div>
+        <p>
+            Unvermeidbare Flüge kompensieren! Klimaschädliche Urlaubsflüge sind schon ein bisschen fragwürdig.
+        </p>
     </div>
 </template>
